0c72c3eed869a0789e6c792918a0952143d85a06
Igor Wicha <130784433+xigor1337@users.noreply.github.com>
2024-07-08 14:38:53 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,32 +1,59 @@
 import tkinter as tk
 from tkinter import messagebox
+import matplotlib.pyplot as plt
 
-class main:
+WINDOWS_SIZE = "800x800"
+MAIN_COLOR = "#525252"
+TEXT_COLOR = "#d9d9d9"
+
+class Template():
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("800x800")
+        self.root.geometry(WINDOWS_SIZE)
         self.root.title("Calculator")
+        self.root.configure(bg=MAIN_COLOR)
 
         # top bar functions
-        self.menubar = tk.Menu(self.root)
+        self.menubar = tk.Menu(self.root, bg=MAIN_COLOR)
 
-        self.filemenu = tk.Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Close", command=exit)
-        self.filemenu.add_separator()
+        self.filemenu = tk.Menu(self.menubar, tearoff=0, bg=MAIN_COLOR, fg=TEXT_COLOR)
+        self.filemenu.add_command(label="Export history", command=exit)
         self.filemenu.add_command(label="Force Close", command=exit)
 
+        self.menusmenu = tk.Menu(self.menubar, tearoff=0, bg=MAIN_COLOR, fg=TEXT_COLOR)
+        self.menusmenu.add_command(label="Calculator", command=self.calculator_option)
+        self.menusmenu.add_command(label="Functions", command=self.functions_option)
+
         self.menubar.add_cascade(menu=self.filemenu, label="File")
+        self.menubar.add_cascade(menu=self.menusmenu, label="Menus")
 
         self.root.config(menu=self.menubar)
+
+    def calculator_option(self):
+        if self.root.state() == 'normal':
+            Main()
+        else:
+            messagebox.showinfo(title="Error", message="You can`t open the same windows twice")
+
+    def functions_option(self):
+        if self.root.state() == 'normal':
+            Functions()
+            print(self.root)
+        else:
+            messagebox.showinfo(title="Error", message="You can`t open the same windows twice")
+
+class Main(Template):
+    def __init__(self):
+        super().__init__()
 
         # calculator
         # display
         self.view = ""
-        self.display = tk.Label(text="")
+        self.display = tk.Label(text="", height=5, font=("Arial", 16), bg=MAIN_COLOR, fg=TEXT_COLOR)
         self.display.pack(pady=10)
 
         # buttons
-        self.calc = tk.Frame(self.root)
+        self.calc = tk.Frame(self.root, bg=MAIN_COLOR)
         self.calc.columnconfigure(0, weight=1)
         self.calc.columnconfigure(1, weight=1)
         self.calc.columnconfigure(2, weight=1)
@@ -55,8 +82,8 @@ class main:
         self.root.mainloop()
 
     def button_create(self, text, row, col, command):
-        self.btn = tk.Button(self.calc, text=text, command=command)
-        self.btn.grid(row=row, column=col, sticky=tk.W + tk.E)
+        self.btn = tk.Button(self.calc, text=text, command=command, height=2, bg="#d6e2ff", bd=1, relief="solid", font=("Arial", 16))
+        self.btn.grid(row=row, column=col, sticky=tk.W + tk.E, padx=2, pady=2)
 
     def send(self, value):
         self.view += value
@@ -118,13 +145,13 @@ class main:
 
     def solution(self):
         #check for errors
-        if self.view[0].isdecimal() == False or self.view[-1].isdecimal() == False:
+        if (self.view[0].isdecimal() == False and self.view[0] != "-") or self.view[-1].isdecimal() == False:
             messagebox.showinfo(title="Error", message="Calculation must start and end with a number")
         else:
             # change to list
             current_numer = ""
             list = []
-            for object in self.view:
+            for object in self.view.strip():
                 if object.isdecimal() or object == "*" or object == "/":
                     current_numer += object
                 else:
@@ -136,6 +163,8 @@ class main:
             print(list)
 
             # calculate multiplication and division
+            if list[0] == '':
+                list[0] = "0"
             list2 = []
             for object in list:
                 if object.isdecimal() or object == "+" or object == "-":
@@ -182,4 +211,83 @@ class main:
         self.view = ""
         self.display.config(text=self.view)
 
-main()
+class Functions(Template):
+    def __init__(self):
+        super().__init__()
+
+        self.inputframe = tk.Frame(self.root)
+        self.inputframe.columnconfigure(0, weight=1)
+
+        self.title = tk.Label(self.root, text="Input function formula here:", font=("Arial", 16), bg=MAIN_COLOR, fg=TEXT_COLOR)
+        self.title.pack()
+
+        # formula inputs
+        self.input_formula = tk.Entry(self.root, font=("Arial", 16), bg=MAIN_COLOR, relief="solid", justify='center')
+        self.input_formula.pack(fill="x")
+
+
+        # range inputs
+        self.range_frame = tk.Frame(self.root)
+        self.range_frame.columnconfigure(0, weight=1)
+        self.range_frame.columnconfigure(1, weight=1)
+
+        self.range_left_label = tk.Label(self.range_frame, text="Left part of input range", font=("Arial", 16), bg=MAIN_COLOR, fg=TEXT_COLOR)
+        self.range_left_label.grid(row=0, column=0, sticky=tk.W + tk.E)
+        self.range_left = tk.Entry(self.range_frame, font=("Arial", 16), bg=MAIN_COLOR, relief="solid", justify='center')
+        self.range_left.grid(row=1, column=0, sticky=tk.W + tk.E)
+
+        self.range_right_label = tk.Label(self.range_frame, text="Right part of input range", font=("Arial", 16), bg=MAIN_COLOR, fg=TEXT_COLOR)
+        self.range_right_label.grid(row=0, column=1, sticky=tk.W + tk.E)
+        self.range_right = tk.Entry(self.range_frame, font=("Arial", 16), bg=MAIN_COLOR, relief="solid", justify='center')
+        self.range_right.grid(row=1, column=1, sticky=tk.W + tk.E)
+
+        self.range_frame.pack(fill="x")
+
+        self.btn = tk.Button(self.root, text="plot", command=self.make_function, width=20, height=2, bg="#fec3c3", relief="flat")
+        self.btn.pack(pady=5)
+
+        self.root.mainloop()
+
+    def make_function(self):
+        formula = self.input_formula.get()
+        range_left = self.range_left.get()
+        range_right = self.range_right.get()
+
+        # range error check
+        correct = True
+        if range_left[0].isdecimal() == False and range_left[0] != "-":
+            messagebox.showinfo(title="Error", message="There is invalid symbol in the left range")
+            correct = False
+        for i in range_left[1:].strip():
+            if i.isdecimal() == False:
+                messagebox.showinfo(title="Error", message="There is invalid symbol in the left range")
+                correct = False
+                break
+        for i in range_right.strip():
+            if i.isdecimal() == False:
+                messagebox.showinfo(title="Error", message="There is invalid symbol in the right range")
+                correct = False
+                break
+
+        # formula error check
+        allow = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "x"]
+        for i in formula.strip():
+            if i not in allow:
+                messagebox.showinfo(title="Error", message="There is invalid symbol in the formula")
+                correct = False
+                break
+
+        # plot calculation
+        if correct == True:
+            # plot calculation
+            x_axis = []
+            y_axis = []
+            for x in range(int(range_left.strip()), int(range_right.strip()) + 1):
+                y = eval(formula.strip())
+                # print("x =", x, "y =", y)
+                x_axis.append(x)
+                y_axis.append(y)
+            plt.plot(x_axis, y_axis, "ro")
+            plt.show()
+
+Main()
